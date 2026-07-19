45cbbb1756199d9433fdb380d520f326c9e62320
Daniel Felix <felixdaniel-developer@outlook.com>
2026-07-18 21:56:04 -0300

    feat: implement book management module

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,35 @@ import { AutorService } from "./services/AutorService";
 import { AutorController } from "./controllers/AutorController";
 import { AutorMenu } from "./menus/AutorMenu";
 
+import { LivroRepository } from "./repositories/LivroRepository";
+import { LivroService } from "./services/LivroService";
+import { LivroController } from "./controllers/LivroController";
+import { LivroMenu } from "./menus/LivroMenu";
+
 async function main(): Promise<void> {
   try {
+    // ===== MÓDULO DE AUTORES =====
+
     const autorRepository = new AutorRepository();
-    const autorService = new AutorService(autorRepository);
-    const autorController = new AutorController(autorService);
-    const autorMenu = new AutorMenu(autorController);
+    // const autorService = new AutorService(autorRepository);
+    // const autorController = new AutorController(autorService);
+    // const autorMenu = new AutorMenu(autorController);
 
-    await autorMenu.mostrarMenu();
+    // ===== MÓDULO DE LIVROS =====
 
-    autorMenu.fechar();
+    const livroRepository = new LivroRepository();
+
+    const livroService = new LivroService(livroRepository, autorRepository);
+
+    const livroController = new LivroController(livroService);
+    const livroMenu = new LivroMenu(livroController);
+
+    // ===== INÍCIO DA APLICAÇÃO =====
+
+    await livroMenu.mostrarMenu();
+
+    livroMenu.fechar();
+    // autorMenu.fechar();
   } catch (error) {
     if (error instanceof Error) {
       console.error(`Erro ao iniciar a aplicação: ${error.message}`);
